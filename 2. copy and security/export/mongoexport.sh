5b83b967e1c0010deb52ae2bd7087431aab0a23d
dcortesnet <dcortes.net@gmail.com>
2023-07-06 19:30:22 -0400

    Copy and security

---
+++ 2. copy and security/export/mongoexport.sh
@@ -0,0 +1,1 @@
+mongoexport --uri="mongodb://localhost:27017/database"  --collection=users  --out=users.json
---
+++ 2. copy and security/import/mongoimport.sh
@@ -0,0 +1,1 @@
+mongoimport --uri mongodb://localhost:27017/database --collection users --type json --file users.json
